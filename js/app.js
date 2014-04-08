8cdb303914aa96b159bbe6371cd6d9a490a2f599
Zachary Spector <zjspector@gmail.com>
2014-04-08 14:34:35 -0400

    Add randNum function and newGame function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
-	
+	var answer;
+	newGame();
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -14,4 +15,20 @@ $(document).ready(function(){
 
 });
 
+function newGame() {
+	console.log(randNum());
+	answer = randNum();
+	//Reset #feedback to "Make your Guess!"
+	//Empty #guesslist
+	//Reset #count to 0
+}
 
+function randNum() {
+	return Math.floor(Math.random() * 101);
+}
+
+function guess(i) {
+	//Change feedback depending on proximity
+	//Append li with input onto #guesslist
+	//Change #count content
+}
