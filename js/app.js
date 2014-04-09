3bf8037d90b0bf187f107810332e09aa329c8990
Zachary Spector <zjspector@gmail.com>
2014-04-09 13:34:37 -0400

    Complete game in js
    Needs fixed CSS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-	var answer;
 	newGame();
 	$('.new').click(function() {
 		newGame();
@@ -17,51 +16,82 @@ $(document).ready(function(){
 		});
 
 		/*--- Game play functions ---*/
-		$("#userButton").click(function() {
-			submitGuess();
+		$("#guessButton").click(function(event) {
+			submitGuess(event);
 		});
 });
+var answer = 0;
+var count = 0;
+var guess = 0;
+var guessArray = [];
 
 function newGame() {
-	console.log(randNum());
 	answer = randNum();
+	console.log(answer);
 	//Reset #feedback to "Make your Guess!"
+	$('#feedback').text("Make your Guess!");
 	//Empty #guesslist
+	$("#guessList").empty();
 	//Reset #count to 0
+	count = 0;
+	$("#count").text(count);
+	guessArray = [];
 }
 
 function randNum() {
 	return Math.floor(Math.random() * 101);
 }
 
-function submitGuess() {
-	var guess = +$('#userGuess').val();
-	if (guess%1 === 0 && guess > 0 && guess < 101) {
-		//Change feedback depending on proximity
-		var temp = Math.abs(guess - answer);
-		var feedback = $('#feedback');
-		if (guess === answer) {
-			feedback.text("You got it!");
-		} else if (temp >= 1 && temp <= 5) {
-			feedback.text("Burning!");
-		} else if (temp >= 6 && temp <= 10) {
-			feedback.text("Very hot");
-		} else if (temp >= 11 && temp <= 15) {
-			feedback.text("Hot");
-		} else if (temp >= 16 && temp <= 20) {
-			feedback.text("Warm");
-		} else if (temp >= 21 && temp <= 30) {
-			feedback.text("Cold");
-		} else if (temp >= 31 && temp <= 40) {
-			feedback.text("Very cold");
-		} else {
-			feedback.text("Freezing!");
-		}
-		//Append li with input onto #guesslist
-		$('#guessList').append('<li>' +guess+ '</li>');
-		//Change #count content
-		var count = 0;
-		count++;
-		$('#count').text(count);
+function addCount() {
+	count++;
+	$('#count').text(count);
+}
+
+function addGuess() {
+	
+	$('#guessList').append('<li>' +guess+ '</li>');
+	$('#userGuess').val('');
+}
+
+function feedback() {
+	var temp = Math.abs(guess - answer);
+	var feedback = $('#feedback')
+	if (guess === answer) {
+		feedback.text("You got it!");
+	} else if (temp >= 1 && temp <= 5) {
+		feedback.text("Burning!");
+	} else if (temp <= 10) {
+		feedback.text("Very hot");
+	} else if (temp <= 15) {
+		feedback.text("Hot");
+	} else if (temp <= 20) {
+		feedback.text("Warm");
+	} else if (temp <= 30) {
+		feedback.text("Cold");
+	} else if (temp <= 40) {
+		feedback.text("Very cold");
+	} else {
+		feedback.text("Freezing!");
+	}
+}
+
+function pastGuess() {
+		guessArray.push(guess);
+}
+
+function submitGuess(event) {
+	event.preventDefault();
+	guess = +$('#userGuess').val();
+	if (guessArray.indexOf(guess) === -1 && guess > 0 && guess < 101 && guess % 1 === 0) {
+		pastGuess();
+		feedback();
+		addGuess();
+		addCount();
+	} else if (guessArray.indexOf(guess) != -1) {
+		alert("You already guessed that number!");
+		$('#userGuess').val('');
+	} else {
+		alert("Please choose a valid number between 1 and 100");
+		$('#userGuess').val('');
 	}
 }
